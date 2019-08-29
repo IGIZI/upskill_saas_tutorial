d18cb8e2010303b33dc43346fb4a269fb4d950d2
Niko Ikaheimo <niko.ikaheimo@gmail.com>
2019-08-29 15:43:35 +0000

    added comments to contact form code

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,34 @@
 class ContactsController < ApplicationController
+    
+    # GET request to /contact-us
+    # Show new contact form
     def new
         @contact = Contact.new
     end
     
+    
+    #POST request for /contacts
     def create
+      # Mass assigment of form field into contact object
       @contact = Contact.new(contact_params)
+      # Save contact object to database
       if @contact.save
+        # Store form field via parameters, into variables
         name = params[:contact][:name]
         email = params[:contact][:email]
         body = params[:contact][:comments]
+        # Plug variables into contact mailer
+        # email method and send email
         ContactMailer.contact_email(name, email, body).deliver
+        # Store success message in flash hash
+        # and redirect to new action
         flash[:success] = "Message sent!"
         redirect_to new_contact_path
         
       else
+        # If form doesn't save
+        # store error into flash hash
+        # and redirect to new action
         flash[:danger] = @contact.errors.full_messages.join(", ")
         redirect_to new_contact_path
         
@@ -22,6 +37,8 @@ class ContactsController < ApplicationController
     end
     
     private
+    # To collect data from form, we need to use
+    # strong parameters and whitelist the form fields
       def contact_params
         params.require(:contact).permit(:name, :email, :comments)
       end
